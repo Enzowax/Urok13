d56295998a1df8c34a080a36e258f6d8d3a63916
strea <strea@DESKTOP-VM30IQ2>
2024-10-10 08:18:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <windows.h> 
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int r = 3;
+    const int c = 3;
+    int array[r][c] = {
+        {-1, 2, -3},
+        {4, -5, 6},
+        {-7, 8, -9}
+    };
+
+    int sum = 0;
+    int positiveCount = 0, negativeCount = 0;
+
+    cout << "Положительные: ";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            sum += array[i][j];
+            if (array[i][j] > 0) {
+                cout << array[i][j] << " ";
+                positiveCount++;
+            }
+        }
+    }
+
+    cout << "\nОтрицательные: ";
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (array[i][j] < 0) {
+                cout << array[i][j] << " ";
+                negativeCount++;
+            }
+        }
+    }
+
+    int totalElements = r * c;
+    double average = static_cast<double>(sum) / totalElements;
+
+    cout << "\n\nСумма: " << sum << endl;
+    cout << "Среднее: " << average << endl;
+    cout << "Всего положительных: " << positiveCount << endl;
+    cout << "Всего отрицательных: " << negativeCount << endl;
+
+    return 0;
+}
